a17fc4cbb81df737bee0ed2b42c983cd4e9fc11a
Ahmed Magdy <ahmed.developer.full.stack>
2025-09-01 03:27:02 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,32 @@
+import { Elysia } from "elysia";
+import cors from "@elysiajs/cors";
+import { getProfilesHandler, deleteProfileHandler, getProfileByIdHandler, updatePasswordHandler } from "./routes/getProfiles";
+import { registerHandler } from "./routes/register";
+import { loginHandler } from "./routes/login";
+import { examsRoutes } from "./routes/exams";
+import { questionsRoutes } from "./routes/questions";
+import { resultsRoutes } from "./routes/resultRoutes";
+import { attachmentsRoutes } from "./routes/attachments";
+import { adminsRoutes } from "./routes/admins";
+
+const app = new Elysia();
+
+app.use(cors({ origin: "*" }));
+
+// Profiles
+app.get("/profiles", getProfilesHandler);
+deleteProfileHandler(app);
+getProfileByIdHandler(app);
+updatePasswordHandler(app);
+
+app.post("/register", registerHandler);
+app.post("/login", loginHandler);
+
+examsRoutes(app);
+questionsRoutes(app);
+resultsRoutes(app);
+attachmentsRoutes(app);
+adminsRoutes(app);
+
+app.listen(3000);
+console.log("🚀 Backend running on http://localhost:3000");
